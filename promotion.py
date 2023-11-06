8e0d15c47a9b8781886662422da3821e6a6b8110
Xenon <78194120+XenonChan@users.noreply.github.com>
2023-11-06 14:58:43 +0700

    Update promotion.py

--- promotion.py
+++ promotion.py
@@ -13,3 +13,39 @@ for j in m_list:
 least_table = remaining // n
 
 print(least_table)
+
+def make_pearl(word):
+  """
+  สร้างเพรชจากคำ
+
+  Args:
+    word: คำที่ต้องการสร้างเพรช
+
+  Returns:
+    เพรชจากคำ
+  """
+
+  # แยกคำออกเป็นตัวอักษร
+  characters = list(word)
+
+  # กำหนดตัวอักษรเริ่มต้น
+  current_character = characters[len(characters) // 2]
+
+  # สร้างเพรช
+  pearl = ""
+  for i in range(len(characters)):
+    # เพิ่มตัวอักษรข้างๆที่ล่ะหนึ่ง
+    if i < len(characters) - 1:
+      pearl += characters[i]
+
+    # เพิ่มตัวอักษรตรงกลางสุดแบบอยู่ข้างล่าง
+    if i > 0:
+      pearl += current_character
+
+    # เปลี่ยนตัวอักษรเริ่มต้นเป็นตัวอักษรถัดไป
+    current_character = characters[(i + len(characters) // 2) % len(characters)]
+
+  return pearl
+
+
+print(make_pearl("hello"))
