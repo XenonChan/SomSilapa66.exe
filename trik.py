7d94976da73695123b0f19148ac74e5629a5164d
Xenon <78194120+XenonChan@users.noreply.github.com>
2023-11-07 10:13:50 +0700

    Update trik.py

--- trik.py
+++ trik.py
@@ -13,3 +13,43 @@ for i in pattern:
 for j in range(3):
   if ls[j] == 1:
     print(j + 1)
+
+
+def make_diamond_hole(word):
+  """
+  สร้างรูทรงเพชรจากคำ
+
+  Args:
+    word: คำที่ต้องการสร้างรู
+
+  Returns:
+    รูทรงเพชรจากคำ
+  """
+
+  # ตรวจสอบความยาวของคำ
+
+  if len(word) <= 2:
+    return word
+
+  # สร้างรูทรงเพชร
+
+  diamond_hole = []
+  for i in range(len(word)):
+    if i == 0:
+      diamond_hole.append(" " * (len(word) - 1))
+      diamond_hole.append(word[i])
+    else:
+      diamond_hole.append(" " * (len(word) - i - 2))
+      diamond_hole.append(word[i])
+  for i in range(len(word) - 2, -1, -1):
+    if i == 0:
+      diamond_hole.append(word[i])
+      diamond_hole.append(" " * (len(word) - 1))
+    else:
+      diamond_hole.append(word[i])
+      diamond_hole.append(" " * (len(word) - i - 2))
+
+  return "".join(diamond_hole)
+
+
+print(make_diamond_hole("BEDROOM"))
