d5ce6d655cfa3b9ed03944feec40d9c9b7f90bf4
Xenon <78194120+XenonChan@users.noreply.github.com>
2023-11-08 11:36:46 +0700

    Update trik.py

--- trik.py
+++ trik.py
@@ -14,5 +14,29 @@ for j in range(3):
   if ls[j] == 1:
     print(j + 1)
 
-nums_decen = int("".join(map(str, [hundred, hundred - 1, hundred - 2])))
-ValueError: invalid literal for int() with base 10: '0-1-2'
+n = int(input("ป้อนค่า n (มากกว่า 100 แต่ไม่เกิน 1000): "))
+
+if 100 < n <= 1000:
+    # สร้างรายการเริ่มจาก 100 ไปถึง n
+    ascending_numbers = list(range(100, n + 1))
+    # สร้างรายการเริ่มจาก n ลงมาถึง 100
+    descending_numbers = list(range(n, 99, -1))
+    
+    # แสดงเลขที่เรียงจากน้อยไปมาก
+    print("เลขที่เรียงจากน้อยไปมาก:", ascending_numbers)
+    # แสดงเลขที่เรียงจากมากไปน้อย
+    print("เลขที่เรียงจากมากไปน้อย:", descending_numbers)
+
+    # ลูปและแสดงเลขระหว่าง 100 ถึง n โดยเพิ่มหรือลดทีละ 1
+    print("เลขที่เรียงโดยเพิ่มหรือลดทีละ 1:")
+    current_number = 100
+    while current_number != n:
+        print(current_number, end=" ")
+        if current_number < n:
+            current_number += 1
+        else:
+            current_number -= 1
+    print(n)  # แสดงค่า n ที่ถูกค้นพบ
+else:
+    print("กรุณาป้อนค่า n ที่อยู่ในช่วง 101 ถึง 1000")
+
