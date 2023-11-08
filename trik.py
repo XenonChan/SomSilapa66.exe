45aa7232351debc33e364b3f77d332cad4715af3
Xenon <78194120+XenonChan@users.noreply.github.com>
2023-11-08 13:07:29 +0700

    Update trik.py

--- trik.py
+++ trik.py
@@ -14,29 +14,28 @@ for j in range(3):
   if ls[j] == 1:
     print(j + 1)
 
-n = int(input("ป้อนค่า n (มากกว่า 100 แต่ไม่เกิน 1000): "))
 
-if 100 < n <= 1000:
-    # สร้างรายการเริ่มจาก 100 ไปถึง n
-    ascending_numbers = list(range(100, n + 1))
-    # สร้างรายการเริ่มจาก n ลงมาถึง 100
-    descending_numbers = list(range(n, 99, -1))
-    
-    # แสดงเลขที่เรียงจากน้อยไปมาก
-    print("เลขที่เรียงจากน้อยไปมาก:", ascending_numbers)
-    # แสดงเลขที่เรียงจากมากไปน้อย
-    print("เลขที่เรียงจากมากไปน้อย:", descending_numbers)
 
-    # ลูปและแสดงเลขระหว่าง 100 ถึง n โดยเพิ่มหรือลดทีละ 1
-    print("เลขที่เรียงโดยเพิ่มหรือลดทีละ 1:")
-    current_number = 100
-    while current_number != n:
-        print(current_number, end=" ")
-        if current_number < n:
-            current_number += 1
-        else:
-            current_number -= 1
-    print(n)  # แสดงค่า n ที่ถูกค้นพบ
+def is_increasing(number):
+    return all(number[i] <= number[i + 1] for i in range(len(number) - 1))
+
+def is_decreasing(number):
+    return all(number[i] >= number[i + 1] for i in range(len(number) - 1))
+
+def find_increasing_decreasing_numbers(n):
+    numbers = []
+    for num in range(100, n + 1):
+        num_str = str(num)
+        if is_increasing(num_str) or is_decreasing(num_str):
+            numbers.append(num_str)
+    return numbers
+
+n = int(input("ป้อนค่า n (100-999): "))
+if 100 <= n < 1000:
+    result = find_increasing_decreasing_numbers(n)
+    if result:
+        print("ตัวเลขที่เรียงแบบบวกหรือแบบลดลง:", result)
+    else:
+        print("ไม่พบตัวเลขที่เรียงแบบบวกหรือแบบลดลงในช่วงนี้")
 else:
-    print("กรุณาป้อนค่า n ที่อยู่ในช่วง 101 ถึง 1000")
-
+    print("โปรดป้อนค่า n ในช่วง 100-999 เท่านั้น")
