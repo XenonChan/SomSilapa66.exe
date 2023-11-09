29718bc24e33239978bdea6d548fa5314208a992
Xenon <78194120+XenonChan@users.noreply.github.com>
2023-11-09 15:07:49 +0700

    Add files via upload

---
+++ numstair.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+current_value = 10
+array = []
+
+for i in range(1, n + 1):
+    row = [current_value + j for j in range(i)]
+    array.append(row)
+    current_value += 10
+    
+for j in array:
+    print(" ".join(map(str, j)))
